fc0e260078449363b7d6fb4c842402b77609c3e8
tigermap <tigermap7@gmail.com>
2020-09-08 23:47:54 +0900

    클라이언트 관리를 위한 EchoHandler 핸들러 클래스 작성

--- src/main/java/com/kh/runningdog/util/EchoHandler.java
+++ src/main/java/com/kh/runningdog/util/EchoHandler.java
@@ -1,5 +1,40 @@
 package com.kh.runningdog.util;
 
-public class EchoHandler {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class EchoHandler extends TextWebSocketHandler{
+	//private Map<String, WebSocketSession> sessions = new HashMap<String, WebSocketSession>();
+	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
+	private static Logger logger = LoggerFactory.getLogger(EchoHandler.class);
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		// sessions.put(session.getId(), session);
+		sessionList.add(session);
+		logger.info("{} 연결됨", session.getId());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		logger.info("{}로 부터 {} 받음", session.getId(), message.getPayload());
+		
+		for (WebSocketSession sess : sessionList) {
+			sess.sendMessage(new TextMessage(session.getPrincipal().getName() + "|" + message.getPayload()));
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		sessionList.remove(session);
+		
+		logger.info("{} 연결 끊김", session.getId());
+	}
 }
