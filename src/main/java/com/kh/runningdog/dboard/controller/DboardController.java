676c9314bfd08a8b983e16ce24ea93e3e0ecd709
HWAJONG LEE <wowlover10@nate.com>
2020-09-08 19:56:02 +0900

    animalList.do 연결 jsp 오타 수정

--- src/main/java/com/kh/runningdog/dboard/controller/DboardController.java
+++ src/main/java/com/kh/runningdog/dboard/controller/DboardController.java
@@ -1,5 +1,22 @@
 package com.kh.runningdog.dboard.controller;
 
-public class DboardController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kh.runningdog.dboard.model.service.DboardService;
+
+@Controller
+public class DboardController {
+	private static final Logger logger = LoggerFactory.getLogger(DboardController.class);
+	
+	@Autowired
+	private DboardService dboardService;
+	
+	@RequestMapping("dlistPage.do")
+	public String moveDlistPage() {
+		return "animal/animalList";
+	}
 }
