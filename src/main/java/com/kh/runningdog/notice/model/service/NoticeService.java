f73e94877bc45a21f03964fba698634ff30238e3
DESKTOP-4RII3A9\true0 <tiger275458@gmail.com>
2020-09-09 16:28:31 +0900

    공지사항 service 전체 메소드 추가

--- src/main/java/com/kh/runningdog/notice/model/service/NoticeService.java
+++ src/main/java/com/kh/runningdog/notice/model/service/NoticeService.java
@@ -1,5 +1,14 @@
 package com.kh.runningdog.notice.model.service;
 
-public interface NoticeService {
+import java.util.ArrayList;
 
+import com.kh.runningdog.notice.model.vo.Notice;
+
+public interface NoticeService {
+	ArrayList<Notice> selectNoticeList();
+	Notice selectNoticeOne(int noticeNo);
+	ArrayList<Notice> selectNoticeStateList();
+	int insertNotice(Notice notice);
+	int updateNotice(Notice notice);
+	int deleteNotice(int noticeNo);
 }
--- src/main/java/com/kh/runningdog/notice/model/service/NoticeServiceImpl.java
+++ src/main/java/com/kh/runningdog/notice/model/service/NoticeServiceImpl.java
@@ -1,15 +1,53 @@
 package com.kh.runningdog.notice.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.runningdog.notice.model.dao.NoticeDao;
+import com.kh.runningdog.notice.model.vo.Notice;
 
 @Service("noticeService")
 public class NoticeServiceImpl implements NoticeService{
 
 	@Autowired
 	private NoticeDao noticeDao;
+
+	@Override
+	public ArrayList<Notice> selectNoticeList() {
+		return noticeDao.selectNoticeList();
+	}
+
+	@Override
+	public Notice selectNoticeOne(int noticeNo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Notice> selectNoticeStateList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertNotice(Notice notice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateNotice(Notice notice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteNotice(int noticeNo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
 	
 	
