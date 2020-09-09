3648f89f6149abc47fcb1b906e45444c94c05a40
tigermap <tigermap7@gmail.com>
2020-09-09 17:37:34 +0900

    FileWriter test

--- src/main/java/com/kh/runningdog/websocket/util/EchoHandler.java
+++ src/main/java/com/kh/runningdog/websocket/util/EchoHandler.java
@@ -1,5 +1,7 @@
 package com.kh.runningdog.websocket.util;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -15,35 +17,59 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @RequestMapping("/echo")
-public class EchoHandler extends TextWebSocketHandler{
-	//private Map<String, WebSocketSession> sessions = new HashMap<String, WebSocketSession>();
+public class EchoHandler extends TextWebSocketHandler {
+	// private Map<String, WebSocketSession> sessions = new HashMap<String,
+	// WebSocketSession>();
 	private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
 	private static Logger logger = LoggerFactory.getLogger(EchoHandler.class);
-	
+
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		// sessions.put(session.getId(), session);
 		sessionList.add(session);
 		logger.info("{} 연결됨", session.getId());
-		
+
 		logger.info("인터셉터 : " + getHttpSessionValue(session));
 	}
-	
+
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		logger.info("{}로 부터 {} 받음", getHttpSessionValue(session), message.getPayload());
-		
+
 		for (WebSocketSession sess : sessionList) {
-			sess.sendMessage(new TextMessage(getHttpSessionValue(session)/* session.getPrincipal().getName() */ + "|" + message.getPayload()));
+			sess.sendMessage(new TextMessage(
+					getHttpSessionValue(session)/* session.getPrincipal().getName() */ + "|" + message.getPayload()));
+			//String txt = "[" + getHttpSessionValue(session) + "]" + message.getPayload();
+			String txt = "테스트입니다";
+			logger.info(txt);
+			String path = System.getProperty("user.home") + "/Desktop/tigermap/";
+			String fileName = "text11.txt";
+			File folder = new File(path);
+			if (!folder.exists()) {
+				folder.mkdir();
+			}
+				try {
+					// 파일 객체 생성
+					// true 지정시 파일의 기존 내용에 이어서 작성
+					File file = new File(path + fileName);
+					FileWriter fw = new FileWriter(file, true);
+					// 파일안에 문자열 쓰기
+					fw.write(txt);
+					fw.flush();
+					// 객체 닫기
+					fw.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 		}
 	}
-	
+
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		sessionList.remove(session);
 		logger.info("{} 연결 끊김", getHttpSessionValue(session));
 	}
-	
+
 	public Object getHttpSessionValue(WebSocketSession session) {
 		Map<String, Object> map = session.getAttributes();
 		Object value = map.get("userid");
