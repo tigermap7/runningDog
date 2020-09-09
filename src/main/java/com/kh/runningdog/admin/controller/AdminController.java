eabec134e5509f716efe81d3598fdb6d533b1117
dydhslxl7 <dydhslxl7@naver.com>
2020-09-09 09:30:16 +0900

    컨트롤러 패키지, 클래스명 변경

--- src/main/java/com/kh/runningdog/admin/controller/AdminController.java
+++
@@ -1,5 +0,0 @@
-package com.kh.runningdog.admin.controller;
-
-public class AdminController {
-
-}
---
+++ src/main/java/com/kh/runningdog/admin/sponsor/controller/AdminSponsorController.java
@@ -0,0 +1,5 @@
+package com.kh.runningdog.admin.sponsor.controller;
+
+public class AdminSponsorController {
+
+}
