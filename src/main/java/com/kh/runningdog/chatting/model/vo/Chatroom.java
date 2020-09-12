4004614464caa8a6cc28a0901d8bae7468eb302a
tigermap <tigermap7@gmail.com>
2020-09-12 12:16:48 +0900

    실시간 채팅 vo클래스 수정 및 추가

--- src/main/java/com/kh/runningdog/chatting/model/vo/Chatroom.java
+++ src/main/java/com/kh/runningdog/chatting/model/vo/Chatroom.java
@@ -1,19 +1,22 @@
 package com.kh.runningdog.chatting.model.vo;
 
 import java.io.Serializable;
+import java.sql.Date;
 
 public class Chatroom implements Serializable{
 	public static final long serialVersionUID = 31L;
 	
 	private int roomNo;
 	private int memberNo;
+	private Date lastDate;
 	
 	public Chatroom() {}
 
-	public Chatroom(int roomNo, int memberNo) {
+	public Chatroom(int roomNo, int memberNo, Date lastDate) {
 		super();
 		this.roomNo = roomNo;
 		this.memberNo = memberNo;
+		this.lastDate = lastDate;
 	}
 
 	public int getRoomNo() {
@@ -31,10 +34,18 @@ public class Chatroom implements Serializable{
 	public void setMemberNo(int memberNo) {
 		this.memberNo = memberNo;
 	}
+	
+	public Date getLastDate() {
+		return lastDate;
+	}
+
+	public void setLastDate(Date lastDate) {
+		this.lastDate = lastDate;
+	}
 
 	@Override
 	public String toString() {
-		return "Chatroom [roomNo=" + roomNo + ", memberNo=" + memberNo + "]";
+		return "Chatroom [roomNo=" + roomNo + ", memberNo=" + memberNo + ", lastDate=" + lastDate + "]";
 	}
 	
 	
---
+++ src/main/java/com/kh/runningdog/chatting/model/vo/Message.java
@@ -0,0 +1,84 @@
+package com.kh.runningdog.chatting.model.vo;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Message implements Serializable{
+	private static final long serialVersionUID = 32L;
+	
+	private int messageNo;
+	private int roomNo;
+	private int sender;
+	private int receiver;
+	private Date sendTime;
+	private String content;
+	
+	public Message() {}
+	
+	public Message(int messageNo, int roomNo, int sender, int receiver, Date sendTime, String content) {
+		super();
+		this.messageNo = messageNo;
+		this.roomNo = roomNo;
+		this.sender = sender;
+		this.receiver = receiver;
+		this.sendTime = sendTime;
+		this.content = content;
+	}
+
+	public int getMessageNo() {
+		return messageNo;
+	}
+
+	public void setMessageNo(int messageNo) {
+		this.messageNo = messageNo;
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public void setRoomNo(int roomNo) {
+		this.roomNo = roomNo;
+	}
+
+	public int getSender() {
+		return sender;
+	}
+
+	public void setSender(int sender) {
+		this.sender = sender;
+	}
+
+	public int getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(int receiver) {
+		this.receiver = receiver;
+	}
+
+	public Date getSendTime() {
+		return sendTime;
+	}
+
+	public void setSendTime(Date sendTime) {
+		this.sendTime = sendTime;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [messageNo=" + messageNo + ", roomNo=" + roomNo + ", sender=" + sender + ", receiver="
+				+ receiver + ", sendTime=" + sendTime + ", content=" + content + "]";
+	}
+	
+	
+	
+}
