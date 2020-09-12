abb4f3b3e539810de26684f0eb6a5735b5ccfe41
DESKTOP-4RII3A9\true0 <tiger275458@gmail.com>
2020-09-13 01:24:01 +0900

    공지사항 controller 관리자쪽 추가, 등록, 수정 부분 AdminNoticeController로 변경(.do -> .ad), 요청한 곳으로 돌아가게 설정(사용자측에서 요청했으면 사용자 리스트페이지로). limit 각 지정해주기

--- src/main/java/com/kh/runningdog/notice/controller/NoticeController.java
+++ src/main/java/com/kh/runningdog/notice/controller/NoticeController.java
@@ -1,10 +1,7 @@
 package com.kh.runningdog.notice.controller;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,9 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.runningdog.notice.model.service.NoticeService;
@@ -55,8 +49,9 @@ public class NoticeController {
 		noticeSearch.setKeyword(keyword);
 		
 		int listCount = noticeService.selectNoticeListCount(noticeSearch);	//목록 갯수
+		int limit = 6; //한 페이지당 출력할 목록 갯수 지정
 		
-		NoticePage noticePage = new NoticePage(currentPage, listCount); //현재 페이지와 총 갯수 보내서, startPage, endpage등.. 값 만들기
+		NoticePage noticePage = new NoticePage(currentPage, listCount, limit); //현재 페이지와 총 갯수 보내서, startPage, endpage등.. 값 만들기
 
 		//검색값 NoticePage 에 넣기 (mybatis mapper로 보낼때 파라메타값 하나만 보낼 수 있어서)
 		noticePage.setSearch(search);
@@ -120,54 +115,10 @@ public class NoticeController {
 	
 	//공지사항 등록 페이지 이동
 	@RequestMapping(value="ninview.do")
-	public ModelAndView moveNoticeInsert(Notice notice, HttpServletRequest request, ModelAndView mv) {
+	public ModelAndView moveNoticeInsert(HttpServletRequest request, ModelAndView mv) {
 		logger.info("ninview.do run...");
 		mv.setViewName("notice/noticeWrite");
 		return mv;
-	}
-	
-	
-	//공지사항 등록
-	@RequestMapping(value="ninsert.do", method=RequestMethod.POST)
-	public String insertAdminNotice(Notice notice, HttpServletRequest request, @RequestParam Map<String, MultipartFile> fileMap) {
-		logger.info("ninsert.do run...");
-		String returnView = null;
-		int i = 1;
-		
-		String savePath = request.getSession().getServletContext().getRealPath("resources/nupfiles"); //파일 저장할 폴더 위치
-		
-		for(String key : fileMap.keySet()) {
-			if(fileMap.get(key).getOriginalFilename() != "") {
-			String originalFilename = fileMap.get(key).getOriginalFilename();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-			String renamefilename = sdf.format(new java.sql.Date(System.currentTimeMillis())); //현재시간
-			renamefilename += "." + originalFilename.substring(originalFilename.lastIndexOf(".") + 1); //확장자명
-			
-			try {
-				fileMap.get(key).transferTo(new File(savePath + "\\" + renamefilename));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-				switch (i) {
-				case 1 : notice.setNoticeOriginalFilename1(originalFilename);
-					     notice.setNoticeRenameFilename1(renamefilename); break;
-				case 2 : notice.setNoticeOriginalFilename2(originalFilename);
-						 notice.setNoticeRenameFilename2(renamefilename); break;
-				case 3 : notice.setNoticeOriginalFilename3(originalFilename);
-						 notice.setNoticeRenameFilename3(renamefilename); break;
-			}
-				i++;
-			}
-		}
-		
-		if(noticeService.insertNotice(notice) > 0) {
-			returnView = "redirect:/nlist.do";
-		} else {
-			request.setAttribute("message", "새 공지사항 등록 처리 실패");
-			returnView = "common/error";
-		}
-		
-		return returnView;
 	}
 	
 	
@@ -188,128 +139,8 @@ public class NoticeController {
 		return mv;
 	}
 	
-	
-	//공지사항 수정
-	@RequestMapping(value="nupdate.do")
-	public String updateAdminNotice(Notice notice, HttpServletRequest request, @RequestParam Map<String, MultipartFile> fileMap) {
-		logger.info("nupdate.do run...");
-		String returnView = null;
-		String savePath = request.getSession().getServletContext().getRealPath("resources/nupfiles"); //파일 저장할 폴더 위치
-		int i = 1;
-		
-		String deleteFilename1 = request.getParameter("deleteFilename1");
-		String deleteFilename2 = request.getParameter("deleteFilename2");
-		String deleteFilename3 = request.getParameter("deleteFilename3");
-		
-		//삭제할 파일이 넘겨져왔을 때 삭제하기
-		if(!(deleteFilename1 == null || deleteFilename1.length() == 0)) {
-			new File(savePath + "\\" + notice.getNoticeRenameFilename1()).delete();
-			notice.setNoticeOriginalFilename1(null);
-			notice.setNoticeRenameFilename1(null);
-		}
-		if(!(deleteFilename2 == null || deleteFilename2.length() == 0)) {
-			new File(savePath + "\\" + notice.getNoticeRenameFilename2()).delete();
-			notice.setNoticeOriginalFilename2(null);
-			notice.setNoticeRenameFilename2(null);
-		}
-		if(!(deleteFilename3 == null || deleteFilename3.length() == 0)) {
-			new File(savePath + "\\" + notice.getNoticeRenameFilename3()).delete();
-			notice.setNoticeOriginalFilename3(null);
-			notice.setNoticeRenameFilename3(null);
-		}
-		
-		//파일 저장순서 변경하기 (1이 삭제되고 2, 3이 남으면 1, 2 이렇게 변경)
-		if(notice.getNoticeOriginalFilename1() == null) {	//1번이 없는 경우(삭제된 경우)
-			if(notice.getNoticeOriginalFilename2() != null) {	//2번이 있는 경우
-				notice.setNoticeOriginalFilename1(notice.getNoticeOriginalFilename2());
-				notice.setNoticeRenameFilename1(notice.getNoticeRenameFilename2());
-				if(notice.getNoticeOriginalFilename3() != null) {	//3번이 있는 경우
-					notice.setNoticeOriginalFilename2(notice.getNoticeOriginalFilename3());
-					notice.setNoticeRenameFilename2(notice.getNoticeRenameFilename3());
-					notice.setNoticeOriginalFilename3(null);
-					notice.setNoticeRenameFilename3(null);
-				} else {	//3번이 없는 경우
-					notice.setNoticeOriginalFilename2(null);
-					notice.setNoticeRenameFilename2(null);
-				}
-			} else {	//2번도 없는 경우
-				if(notice.getNoticeOriginalFilename3() != null) {	//3번만 있는 경우
-					notice.setNoticeOriginalFilename1(notice.getNoticeOriginalFilename3());
-					notice.setNoticeRenameFilename1(notice.getNoticeRenameFilename3());
-					notice.setNoticeOriginalFilename3(null);
-					notice.setNoticeRenameFilename3(null);
-				}
-			}
-		} else {	//1번이 있는 경우
-			if(notice.getNoticeOriginalFilename2() == null) {	//2번이 없는 경우
-				if(notice.getNoticeOriginalFilename3() != null) {	//3번이 있는 경우
-					notice.setNoticeOriginalFilename2(notice.getNoticeOriginalFilename3());
-					notice.setNoticeRenameFilename2(notice.getNoticeRenameFilename3());
-					notice.setNoticeOriginalFilename3(null);
-					notice.setNoticeRenameFilename3(null);
-				}
-			}
-		}
-		
-		
-		//새로 첨부된 파일 있을 경우 파일 추가하기
-		for(String key : fileMap.keySet()) {
-			if(fileMap.get(key).getOriginalFilename() != "") {
-			String originalFilename = fileMap.get(key).getOriginalFilename();
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-			String renamefilename = sdf.format(new java.sql.Date(System.currentTimeMillis())); //현재시간
-			renamefilename += "." + originalFilename.substring(originalFilename.lastIndexOf(".") + 1); //확장자명
-			
-			try {
-				fileMap.get(key).transferTo(new File(savePath + "\\" + renamefilename));
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-				switch (i) {
-				case 1 : 
-					if(notice.getNoticeOriginalFilename1().length() == 0) {	//1번 파일이 없을 때
-						notice.setNoticeOriginalFilename1(originalFilename);
-						notice.setNoticeRenameFilename1(renamefilename);
-					} else { //1번파일이 있을 때
-						if(notice.getNoticeOriginalFilename2().length() == 0) {	//2번파일이 없을 때
-							notice.setNoticeOriginalFilename2(originalFilename);
-							notice.setNoticeRenameFilename2(renamefilename);							
-						} else {
-							notice.setNoticeOriginalFilename3(originalFilename);
-							notice.setNoticeRenameFilename3(renamefilename);							
-						}
-					}
-					break;
-				case 2 :
-					if(notice.getNoticeOriginalFilename2().length() == 0) {	//2번파일이 없을 때
-						notice.setNoticeOriginalFilename2(originalFilename);
-						notice.setNoticeRenameFilename2(renamefilename);						
-					} else {
-						notice.setNoticeOriginalFilename3(originalFilename);
-						notice.setNoticeRenameFilename3(renamefilename); 						
-					}
-					break;
-				case 3 : 
-						notice.setNoticeOriginalFilename3(originalFilename);
-						notice.setNoticeRenameFilename3(renamefilename); 
-					break;
-			}
-				i++;
-			}//if문
-		} //for문
-		
-		if(noticeService.updateNotice(notice) > 0) {
-			returnView = "redirect:/nlist.do";
-		} else {
-			request.setAttribute("message", notice.getNoticeNo() + "번 공지사항 수정 실패");
-			returnView = "common/error";
-		}
-		
-		return returnView;
-	}	
-	
-	
-	//공지사항 삭제
+
+	//공지사항 삭제 (상세페이지에서 각자 삭제했을 경우)
 	@RequestMapping(value="ndelete.do")
 	public String deleteAdminNotice(HttpServletRequest request) {
 		logger.info("ndelete.do run...");
@@ -336,6 +167,9 @@ public class NoticeController {
 	//메인페이지에 필수, new 공지사항 출력하기
 	//@RequestMapping(value="nstate.do")
 	public ModelAndView selectNoticeState(ModelAndView mv) {
+		logger.info("nstate.do run...");
+		ArrayList<Notice> list = noticeService.selectNoticeStateList();
+		
 		return mv;
 	}
 	
